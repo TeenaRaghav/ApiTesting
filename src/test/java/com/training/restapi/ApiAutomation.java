936f58c80bb7c3094cb09e2859567bdf90d342bd
TeenaRaghav <teenaraghav3017@gmail.com>
2024-06-09 14:40:24 -0400

    updated api testing code

--- src/test/java/com/training/restapi/ApiAutomation.java
+++ src/test/java/com/training/restapi/ApiAutomation.java
@@ -11,43 +11,44 @@ import io.restassured.response.Response;
 public class ApiAutomation {
 
 	String sHostURL = "https://us-central1-qa01-tekarch-accmanager.cloudfunctions.net";
-	String sEpLogin ="/login";
-	String sEpgetData ="/getdata";
-	String sURI ;
+	String sEpLogin = "/login";
+	String sEpgetData = "/getdata";
+	String sURI;
 	Response response;
 	String token;
-	
+
 	@Test
 	public void loginUser() {
 //		create the URL
 		sURI = sHostURL + sEpLogin;
 //		assign this to Restassured
-		RestAssured.baseURI=sURI;
-		
-		response=RestAssured.given().body("{\r\n"
+		RestAssured.baseURI = sURI;
+
+		response = RestAssured.given().body("{\r\n"
 				+ "\"username\": \"teenaraghav3017@tekarch.com\",\r\n"
 				+ " \"password\": \"Admin123\"\r\n"
 				+ " }").when().contentType("application/json").post();
-	System.out.println(response.getStatusCode());
+		System.out.println(response.getStatusCode());
 //		to see response
 //		response.prettyPrint();
-	
+
 //		how did we got particular value
 		token = response.jsonPath().get("token[0]");
 		System.out.println(token);
 	}
-	
+
 	@Test
 	public void getData() {
 		sURI = sHostURL + sEpgetData;
 		RestAssured.baseURI = sURI;
-		HashMap<String,String>headerData = new HashMap<String,String>();
+		HashMap<String, String> headerData = new HashMap<String,String>();
 		headerData.put("token", token);
 		headerData.put("Content-Type", "application/json");
 		response = RestAssured.given().headers(headerData).get();
-	List<Object> accountnos = response.jsonPath().get("accountno");
-	for (Object accountno : accountnos) {
-		System.out.println(accountno);
-	}
+		response.prettyPrint();
+//		List<Object> accountnos = response.jsonPath().get("accountno");
+//		for (Object accountno : accountnos) {
+//			System.out.println(accountno);
+//		}
 	}
 }
--- src/test/java/com/training/restapi/ApiAutomationUsers.java
+++ src/test/java/com/training/restapi/ApiAutomationUsers.java
@@ -1,9 +1,19 @@
 package com.training.restapi;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.jayway.jsonpath.JsonPath;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -11,34 +21,98 @@ import static org.hamcrest.Matchers.*;
 
 public class ApiAutomationUsers {
 
+	/* - Status Code 
+	 * - Response body - Schema - structure and key
+	 * - Data in the body
+	 * - Response time is optional .
+	 */
+
+	/* String 
+	 *   equalTo
+	 *   startsWith
+	 *   equalToIgnoreCase
+	 *   EndsWith
+	 *   containsString
+	 *   equaltoIgnoreCaseWhitespace
+	 *   
+	 *   
+	 *   Number:
+	 *   equalTo
+	 *   greaterthan
+	 *   lessThan
+	 *   greaterthanorEqualTo
+	 *   lessthenorEqualTo
+	 *   
+	 *   collections:
+	 *   hasItem
+	 *   hasItems
+	 *   hasKey
+	 *   hadEntry
+*/
 	String sHostUrl ="https://jsonplaceholder.typicode.com/";
 	String sEndpoint = "users";
 	String sURI;
 	
 	@Test
-	public void validateUser() {
+	public void validateUser() throws IOException {
 		sURI =sHostUrl + sEndpoint;
 		RestAssured.baseURI = sURI; // copy paste URI
 		Response response =RestAssured.get();
 		System.out.println(response.getStatusCode());
 //		response.prettyPrint();
-	String Expectedemail = "Sincere@april.biz";
-//	get value
-	String Actualemail = response.jsonPath().get("email[0]");
-	System.out.println(Actualemail);
-	response.then().body("email[0]",equalTo(Expectedemail));
-	response.then().body("email[0]",startsWith("S"));
-	response.then().body("email[0]",endsWith("z"));
-	response.then().body("email[0]",containsString("b"));
-	response.then().body("address.zipcode[0]",equalTo("92998-3874"));
-	response.then().body("address.city",hasItem("Howemouth1"));
+//	get the expected value from json file
+		String path ="C:\\Users\\teena\\Api\\ExpectedResponse.json";
+		String fileReadyToRead = readFileReturnString(path);
+		String Expectedemail= JsonPath.read(fileReadyToRead,"$.[0].email");
+		System.out.println(Expectedemail);
+		String ExpectedCity= JsonPath.read(fileReadyToRead,"$.[0].address.city");
 
-
-
+//		equalTo - Hamcrestmatcher
+		response.then().body("email[0]", equalTo(Expectedemail));	
+		response.then().body("address.city",hasItem(ExpectedCity));
+				
+//	String Expectedemail = "Sincere@april.biz";
+////	get value
+//	String Actualemail = response.jsonPath().get("email[0]");
+//	System.out.println(Actualemail);
+//	response.then().body("email[0]",equalTo(Expectedemail));
+//	response.then().body("email[0]",startsWith("S"));
+//	response.then().body("email[0]",endsWith("z"));
+//	response.then().body("email[0]",containsString("b"));
+//	response.then().body("address.zipcode[0]",equalTo("92998-3874"));
+//	response.then().body("address.city",hasItem("Howemouth1"));
 //	Assert.assertEquals(Actualemail, Expectedemail);
 //	List<String>cities =response.jsonPath().getList("address.city");
 //	for(String city : cities) {
 //		System.out.println(city);
 //	}
+
+		String actualResponse = response.prettyPrint();
+		actualResponse = actualResponse.replace("[", "").replace("]","");
+		validateSchema(actualResponse,"C:\\Users\\teena\\Api\\ExpectedSchema.json");
+		
+		
+	}
+
+	private void validateSchema(String actualResponse, String fileName) throws IOException {
+//		convert response string to json Object
+		JSONObject jsonActual = new JSONObject(new JSONTokener(actualResponse));
+//		Deserialization - converting string to jsonObject
+		
+//		convert the expected schema to a jsonObject
+//		read the content of the json file given in the path
+	String jsonContent = new String(Files.readAllBytes(Paths.get(fileName)));
+	
+	JSONObject jsonExpected = new JSONObject(jsonContent);
+	
+	Schema schema = SchemaLoader.load(jsonExpected);
+	schema.validate(jsonActual);
+//	convert the jsonObject to String -Serialization
+	
+	}
+
+	private String readFileReturnString(String path) throws IOException {
+		byte[] encoded =Files.readAllBytes(Paths.get(path));
+		return new String(encoded,StandardCharsets.UTF_8);
 	}
 }
